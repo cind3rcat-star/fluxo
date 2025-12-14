e4f5ec9dfa701b31c514622bd0b36d8ea28e94af
cind3rcat-star <cind3rcat@gmail.com>
2025-12-13 21:52:04 -0300

    Add fixed characters data to personagens_fixos.js

---
+++ personagens_fixos.js
@@ -0,0 +1,32 @@
+const PERSONAGENS_FIXOS = [
+  { id: 1, nome: "Madara Uchiha", dano: 975, vida: 805, agilidade: 850 },
+  { id: 2, nome: "Neji Hyuga", dano: 745, vida: 2800, agilidade: 685 },
+  { id: 3, nome: "Naruto Sennin", dano: 775, vida: 3040, agilidade: 740 },
+  { id: 4, nome: "Rock Lee", dano: 745, vida: 2100, agilidade: 690 },
+  { id: 5, nome: "Killer Bee", dano: 725, vida: 2680, agilidade: 740 },
+  { id: 6, nome: "Shikamaru Nara", dano: 210, vida: 1400, agilidade: 570 },
+  { id: 7, nome: "Kakuzu", dano: 710, vida: 3200, agilidade: 675 },
+  { id: 8, nome: "Deidara", dano: 785, vida: 2320, agilidade: 650 },
+  { id: 9, nome: "Orochimaru", dano: 630, vida: 2840, agilidade: 740 },
+
+  { id: 20, nome: "Hajime Kashimo", dano: 710, vida: 2560, agilidade: 810 },
+  { id: 21, nome: "Aoi Todo", dano: 610, vida: 2360, agilidade: 725 },
+  { id: 22, nome: "Toji Fushiguro", dano: 810, vida: 3200, agilidade: 780 },
+  { id: 23, nome: "Choso", dano: 715, vida: 2680, agilidade: 650 },
+  { id: 24, nome: "Mahito", dano: 730, vida: 2880, agilidade: 685 },
+  { id: 25, nome: "Ultimate Mechamaru", dano: 780, vida: 3120, agilidade: 510 },
+  { id: 26, nome: "Panda", dano: 450, vida: 2800, agilidade: 310 },
+  { id: 27, nome: "Mechamaru", dano: 650, vida: 1080, agilidade: 560 },
+
+  { id: 40, nome: "Barba Branca", dano: 760, vida: 3220, agilidade: 320 },
+  { id: 41, nome: "Crocodile", dano: 730, vida: 3040, agilidade: 650 },
+  { id: 42, nome: "Portgas D. Ace", dano: 720, vida: 2100, agilidade: 700 },
+  { id: 43, nome: "Jinbe", dano: 745, vida: 3280, agilidade: 505 },
+  { id: 44, nome: "Franky", dano: 700, vida: 3220, agilidade: 370 },
+
+  { id: 60, nome: "Killua Zoldyck", dano: 700, vida: 2600, agilidade: 825 },
+  { id: 61, nome: "Feitan Portor", dano: 730, vida: 2720, agilidade: 760 },
+  { id: 62, nome: "Neferpitou", dano: 710, vida: 2920, agilidade: 780 },
+
+  { id: 80, nome: "Light Yagami", dano: 1, vida: 600, agilidade: 175 }
+];
